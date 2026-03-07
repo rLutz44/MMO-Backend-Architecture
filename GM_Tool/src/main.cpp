65e2ceda31ff26f77b591d6a9ed2bae7d830e08b
rLutz44 <robertlutz35@yahoo.de>
2026-03-07 20:18:01 +0100

    Implementation of Server - Client architecture. Client can also communicate with Server

---
+++ GM_Tool/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <asio.hpp>
+#include "messages.pb.h"
+
+
+int main() {
+
+	asio::io_context io;
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1"), 8080);
+	asio::ip::tcp::socket socket(io);
+	asio::error_code errorcode;
+	socket.connect(endpoint, errorcode);
+
+	if (errorcode) {
+		std::cout << "Failed to conect: " << errorcode.message() << std::endl;
+		return 1;
+	}
+
+	std::cout << "Connected" << std::endl;
+
+	mmo::network::LoginRequest loginRequest;
+	loginRequest.set_username("abc");
+	loginRequest.set_password("123");
+
+	std::string transmission;
+	loginRequest.SerializeToString(&transmission);
+
+	asio::write(socket, asio::buffer(transmission));
+	std::cout << "Message sent" << std::endl;
+
+	io.run();
+
+	return 0;
+
+}
